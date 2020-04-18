1dd16aae7e3a83703b280e49843498463f11439a
KDM_korea <kdm_korea@naver.com>
2020-04-18 14:11:03 +0900

    Fix: 테스트 코드 수정

--- src/server.js
+++ src/server.js
@@ -2,21 +2,4 @@ const http = require('http');
 http.createServer((request, response) =>{
     console.log('hello world');
     const hello = () => console.log('hello world ES6');
-    return request
-
-    .on('error', (err)=>{
-        console.log(err);
-    })
-    .on('data', (data)=>{
-        console.log(data);
-    })
-    .on('end', ()=>{
-        response.on('error', (err)=>{
-            console.log(err);
-        });
-        response.statusCode = 200;
-        response.setHeader('Content-Type', 'text/plain');
-        response.write('hi ');
-        response.end('end');
-    });
 }).listen(8080);
