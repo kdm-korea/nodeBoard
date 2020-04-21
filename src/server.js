bcca61f858553b3057ef78cfaf7e900f621041aa
KDM_korea <kdm_korea@naver.com>
2020-04-21 16:25:35 +0900

    Fix: .js delete

--- src/server.js
+++ src/server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 
 const post = require('./router/post.js');
-app.use('/post', post.js);
+app.use('/post', post);
 
 app.listen(8080, function(){
-
+    
 });
