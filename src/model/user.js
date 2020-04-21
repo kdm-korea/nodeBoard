f2669250903d6f4f11956bb79d9a8b3b862b60bf
KDM_korea <kdm_korea@naver.com>
2020-04-21 16:25:51 +0900

    Delete: user model

--- src/model/user.js
+++
@@ -1,8 +0,0 @@
-const {Sequelize, Model, DataTypes } = require('sequelize');
-const sequelize = new Sequelize('mariadb::memory:');
-
-class User extends Model{}
-User.init({
-    username: DataTypes.STRING,
-    age: DataTypes.INTEGER
-}, {sequelize, modelName: 'user'});
