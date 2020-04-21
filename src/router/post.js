646a5ba184808ab714ed5c483b58046cadc1ffed
KDM_korea <kdm_korea@naver.com>
2020-04-21 15:31:42 +0900

    Feat: Divide router

---
+++ src/router/post.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express.Router();
+
+app.get('/:id', function (req, res) {
+    
+});
+
+app.post('', function (req, res) {
+
+});
+
+app.put('/:id', function (req, res) {
+
+});
+
+app.delete('', function(req, res){
+
+});
+
+module.exports = app;
