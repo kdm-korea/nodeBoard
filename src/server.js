0ceebe6e49384702d5b093ff3ef4ab2633b3e075
KDM_korea <kdm_korea@naver.com>
2020-04-24 16:00:23 +0900

    Fix: change syntex es6

--- src/server.js
+++ src/server.js
@@ -1,10 +1,9 @@
-const express = require('express');
+import express from 'express';
 
 const app = express();
-const port = 8080;
 
 app.get('/', (req, res) => {
   res.send('hello world!');
 });
 
-app.listen(port, () => {});
+app.listen(8080, () => {});
