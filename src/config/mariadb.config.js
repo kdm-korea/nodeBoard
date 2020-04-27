371ebba9cd8d44c0158b0d4f8c651aaf2857556d
KDM_korea <kdm_korea@naver.com>
2020-04-27 15:45:08 +0900

    Feat: config maria db

---
+++ src/config/mariadb.config.js
@@ -0,0 +1,42 @@
+import { readdirSync } from 'fs';
+import { basename as _basename, join } from 'path';
+import Sequelize from 'sequelize';
+// eslint-disable-next-line import/extensions
+// import dir from '../models';
+
+const basename = _basename(__filename);
+const dirname = `${__dirname.substring(0, __dirname.length - 6)}models`;
+
+const db = {};
+
+const sequelize = new Sequelize(
+  process.env.DB_DATABASE_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST_IP,
+    dialect: process.env.DB_DIALECT,
+  }
+);
+
+readdirSync(dirname)
+  .filter((file) => {
+    return (
+      file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
+    );
+  })
+  .forEach((file) => {
+    const model = sequelize.import(join(dirname, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+export default db;
