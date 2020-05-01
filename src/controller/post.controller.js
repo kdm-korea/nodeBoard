3030e857db996f6b0c2b2094fb93fc39b1823c49
KDM_korea <kdm_korea@naver.com>
2020-05-01 22:43:39 +0900

    Feat: promise 사용

--- src/controller/post.controller.js
+++ src/controller/post.controller.js
@@ -1,14 +1,33 @@
-import { boardService } from '../services/post.service';
+import boardService from '../services/post.service';
 
-export const save = async (req) => {
-  const { body } = req;
+const controllerLog = () =>
+  console.log(':::::::::: LEVEL_2 DEBUG LOG :::::::::: Controller');
 
+const savePost = async (req, res, next) => {
   // TODO: 바디 데이터 검사
-  return boardService.getOne(body);
+  controllerLog();
+  await boardService.create(req.body).then((data) => {
+    res.json(data);
+  });
+  next();
 };
 
-export const postDelete = async (req) => {
-  const { body } = req;
+const findOne = async (req, res, next) => {
+  controllerLog();
+  await boardService.findOne(req.params.id).then((post) => res.json(post));
+  next();
+};
 
-  return boardService.delete(body);
+const findAll = async (req, res, next) => {
+  controllerLog();
+  await boardService.findAll().then((posts) => {
+    res.json(posts);
+  });
+  next();
+};
+
+export default {
+  savePost,
+  findOne,
+  findAll,
 };
