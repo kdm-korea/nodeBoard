a6d3f48d9b71009f618c2b5822015974ac7301da
KDM_korea <kdm_korea@naver.com>
2020-05-14 23:34:25 +0900

    Feat: jwt 토큰 유효성 체크

--- src/middleware/auth/jwt.auth.js
+++ src/middleware/auth/jwt.auth.js
@@ -1,14 +1,15 @@
 import jwt from 'jsonwebtoken';
 
-/*
-  1. token create
-  2. token chk effectiveness
-  3. token period
-  */
-const verification = (req, res, next) => {
-  const { token } = req;
-  jwt.verify(token, process.env.JWT_KEY);
-  next();
+const verification = async (req, res, next) => {
+  const { token } = req.query;
+
+  jwt.verify(token, process.env.JWT_KEY, (err, payload) => {
+    if (err) {
+      res.status(400).json({ success: false, message: err.message });
+    }
+    console.log(payload);
+    next();
+  });
 };
 
 export default {
