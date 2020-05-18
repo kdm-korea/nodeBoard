ae8dc180ce53cfc2ee3bc54f03d6955ea689ce4b
KDM_korea <kdm_korea@naver.com>
2020-05-18 20:26:21 +0900

    Feat: yaml파일로 분리

--- src/product/post/post.router.js
+++ src/product/post/post.router.js
@@ -6,52 +6,6 @@ import userController from '../user/user.controller';
 
 const router = express.Router();
 
-/**
- * @swagger
- * definitions:
- *  boardItem:
- *   type: object
- *   required:
- *     - boardTitle
- *     - boardContent
- *     - boardState
- *     - boardType
- *   properties:
- *     id:
- *       type: integer
- *       description: ObjectId
- *     boardTitle:
- *       type: string
- *       description: 게시글 제목
- *     boardContent:
- *       type: string
- *       description: 게시글 내용
- *     boardState:
- *       type: boolean
- *       description: 게시글 숨김상태여부
- *     boardType:
- *       type: string
- *       description: 게시글 타입
- */
-
-/**
- * @swagger
- *  /boards:
- *    get:
- *      tags:
- *      - board
- *      description: 모든 게시글을 가져온다.
- *      produces:
- *      - applicaion/json
- *      parameters:
- *      responses:
- *       200:
- *        description: board of column list
- *        schema:
- *          type: array
- *          items:
- *           $ref: '#/definitions/boardItem'
- */
 router.get('/', postController.findAll);
 
 router.get('/:id', valid.getOneSchema, postController.findOne);
