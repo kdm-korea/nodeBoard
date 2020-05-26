84a8cce530a031422cfe1407c6b37cca71fe7743
KDM_korea <kdm_korea@naver.com>
2020-05-26 22:20:41 +0900

    Fix: signUpSchema 수정

--- src/product/user/valid/user.schema.js
+++ src/product/user/valid/user.schema.js
@@ -1,8 +1,13 @@
-import { check, validationResult } from 'express-validator';
+import { check, validationResult, body, header } from 'express-validator';
 
 const validate = (validations) => {
   return async (req, res, next) => {
-    await Promise.all(validations.map((validation) => validation.run(req)));
+    await Promise.all(
+      validations.map(async (validation) => {
+        const a = await validation.run(req);
+        console.log(a.fields[0]);
+      })
+    );
 
     const errors = validationResult(req);
     if (errors.isEmpty()) {
@@ -12,7 +17,11 @@ const validate = (validations) => {
   };
 };
 
-const email = check('email')
+const token = header('authorization')
+  .notEmpty()
+  .withMessage('토큰이 없습니다.');
+
+const email = body('email')
   .isEmail()
   .not()
   .withMessage('이메일 형식이 아닙니다.')
@@ -20,10 +29,12 @@ const email = check('email')
   .isEmpty()
   .withMessage('이메일은 필수 입력사항입니다.');
 
-const password = check('password')
+const password = body('password')
   .notEmpty()
   .withMessage('비밀번호는 필수 입력사항입니다.');
 
+const name = body('name').notEmpty().withMessage('이름은 필수 입력사항입니다.');
+
 export default {
-  signUpSchema: validate([email, password]),
+  signUpSchema: validate([name, email, password]),
 };
