45f26a535a711df016c3a6bfca371a195647364c
KDM_korea <kdm_korea@naver.com>
2020-05-26 22:22:44 +0900

    Fix: id 대신 hash로 대체

--- src/product/auth/auth.contorller.js
+++ src/product/auth/auth.contorller.js
@@ -3,7 +3,7 @@ import authService from './auth.service';
 
 const execCreateAccessToken = async (req, res) => {
   await userService
-    .findUserById(req.user.id)
+    .findUserByHash(req.user.hash)
     .then((user) => authService.createAccessToken(user))
     .then((accessToken) => res.json({ accesstoken: accessToken }))
     .catch((error) => res.status(401).json({ message: error.message }));
