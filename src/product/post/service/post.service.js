bdc8ddd1ae90f31e9df2c6cd401eb9125727d65a
KDM_korea <kdm_korea@naver.com>
2020-05-27 21:03:48 +0900

    Feat: post service 인덱스

---
+++ src/product/post/service/post.service.js
@@ -0,0 +1,13 @@
+import createPost from './post.create.service';
+import findPostById from './post.findOne.service';
+import updatePost from './post.update.service';
+import deletePost from './post.delete.service';
+import pagingPosts from './post.paging.service';
+
+export default {
+  createPost,
+  findPostById,
+  updatePost,
+  deletePost,
+  pagingPosts,
+};
