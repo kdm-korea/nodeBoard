101d69e34ad42af8b96d6b7d4afe591b6cddd7d5
KDM_korea <kdm_korea@naver.com>
2020-05-27 21:02:50 +0900

    Feat: 글 삭제 서비스

---
+++ src/product/post/service/post.delete.service.js
@@ -0,0 +1,27 @@
+import db from '../../../config/mariadb.config';
+import postHelp from './post.help.service';
+import userService from '../../user/user.service';
+
+const compareWriter = async (postId, password, hash) =>
+  postHelp.findOneById(postId).then(async (record) => {
+    if (record.User.hash !== hash) {
+      throw new Error('글 작성자가 아닙니다.');
+    }
+    await userService.comparePassword(record.User, password);
+
+    return true;
+  });
+
+const execDeletePost = async (postId, password, hash) => {
+  const isWriter = await compareWriter(postId, password, hash);
+  if (isWriter) {
+    return db.Board.destroy({
+      where: {
+        id: postId,
+      },
+    }).then((result) => console.log(result));
+  }
+  throw new Error('???????? Unexception Error');
+};
+
+export default execDeletePost;
