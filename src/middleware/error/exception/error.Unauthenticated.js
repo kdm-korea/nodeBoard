15d20949fb21df00feb81d35c59b0fc733ec92b9
KDM_korea <kdm_korea@naver.com>
2020-06-08 17:23:23 +0900

    Fix: 디렉토리 구조 변경

--- src/middleware/error/exception/error.Unauthenticated.js
+++
@@ -1,9 +0,0 @@
-import BaseError from './error.Base';
-
-class Unauthenticated extends BaseError {
-  constructor(message) {
-    super(401, message || '인증이 완료되지 않았습니다.');
-  }
-}
-
-export default Unauthenticated;
