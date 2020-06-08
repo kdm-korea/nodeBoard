9f3ae50618d7e848a09835df41c56e5156aa5642
KDM_korea <kdm_korea@naver.com>
2020-06-08 17:25:15 +0900

    Feat: Error handling router

--- src/middleware/error/error.handler.js
+++ src/middleware/error/error.handler.js
@@ -1,5 +1,15 @@
+import BaseError from '../../product/help/exception/error.Base';
+
 const ErrorHandler = async (err, req, res, next) => {
-  res.status(err.status).json(err);
+  //Logger
+  //push notification
+  if (err instanceof BaseError) {
+    console.log('Custom Error Exception');
+    res.status(err.status).json(err);
+  } else {
+    console.log('UnException Error Handling');
+    console.log(err);
+  }
 };
 
 export default ErrorHandler;
