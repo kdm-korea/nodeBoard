8c8ab9830867590ef3b40dfa2ec0bd67fcb9d018
KDM_korea <kdm_korea@naver.com>
2020-06-09 17:43:23 +0900

    사용하지 않는 async 삭제

--- src/product/user/service/user.help.service.js
+++ src/product/user/service/user.help.service.js
@@ -1,7 +1,7 @@
 import db from '../../../config/mariadb.config';
-import crypto from '../../../lib/crpytoEncoding';
-import uuid from '../../../lib/uuid';
-import error from '../../../middleware/error';
+import crypto from '../../help/crpytoEncoding';
+import uuid from '../../help/uuid';
+import ErrorMessage from '../../help/exception';
 
 const findUserByHash = async (userHash) => {
   return db.User.findOne({
@@ -11,11 +11,11 @@ const findUserByHash = async (userHash) => {
     if (user !== null) {
       return user;
     }
-    throw new Error(`없는 사용자입니다.`);
+    throw new ErrorMessage.Forbioddan('없는 사용자입니다.');
   });
 };
 
-const findUserByEmail = async (userEmail) => {
+const findUserByEmail = (userEmail) => {
   return db.User.findOne({
     raw: true,
     where: { email: userEmail },
@@ -23,11 +23,11 @@ const findUserByEmail = async (userEmail) => {
     if (user !== null) {
       return user;
     }
-    throw new Error('이메일이 일치하는 유저가 없습니다.');
+    throw new ErrorMessage.Forbioddan('이메일이 일치하는 유저가 없습니다.');
   });
 };
 
-const chkNotExistEamil = async (inputEmail) => {
+const chkNotExistEamil = (inputEmail) => {
   return db.User.findAndCountAll({
     raw: true,
     where: { email: inputEmail },
@@ -35,16 +35,17 @@ const chkNotExistEamil = async (inputEmail) => {
     if (reduplicate.count === 0) {
       return true;
     }
-    throw new error.ConfictError('이미 계정을 소유하고 있는 이메일입니다.');
+    throw new ErrorMessage.Forbioddan(
+      '이미 계정을 소유하고 있는 이메일입니다.'
+    );
   });
 };
 
 const comparePassword = async (user, password) => {
-  console.log(await crypto.comparePassword(password, user.salt, user.password));
   if (await crypto.comparePassword(password, user.salt, user.password)) {
     return true;
   }
-  throw new Error('패스워드가 일치하지 않습니다.');
+  throw new ErrorMessage.Forbioddan('패스워드가 일치하지 않습니다.');
 };
 
 const createUser = async (data) => {
@@ -57,26 +58,30 @@ const createUser = async (data) => {
   return db.User.create(record);
 };
 
-const updateUserInfo = async (user, userdto) => {
-  const authUser = await db.User.findOne({
-    where: { hash: user.hash },
+const updateUserInfo = async (user, userDto) => {
+  const authUser = await db.User.update(
+    { name: userDto.name, email: userDto.email },
+    { where: { hash: user.hash } }
+  ).then((result) => {
+    if (result === 1) {
+      return true;
+    }
+    throw new Error('Database Error, user.help.service :: updateUserInfo');
   });
-  authUser.name = userdto.name;
-  authUser.email = userdto.email;
-  authUser.reload();
-  return true;
 };
 
-const updatePw = async (user, inputPw) => {
-  const authUser = await db.User.findOne({
-    where: { hash: user.hash },
-  });
-  const hashPassword = await crypto.saltHashEncoding(inputPw);
+const updatePw = async (user, newPassword) => {
+  const hashPassword = await crypto.saltHashEncoding(newPassword);
 
-  authUser.password = hashPassword.key;
-  authUser.salt = hashPassword.salt;
-  authUser.reload();
-  return true;
+  return await db.User.update(
+    { password: hashPassword.key, salt: hashPassword.salt },
+    { where: { hash: user.hash } }
+  ).then((changeRecord) => {
+    if (changeRecord[0] === 1) {
+      return true;
+    }
+    throw new Error('Database Error, user.help.service :: updatePw');
+  });
 };
 
 export default {
