6ae6a349e3ec759c5ef4c3a9bfa6f97a8f9c0ec8
KDM_korea <kdm_korea@naver.com>
2020-06-09 17:33:51 +0900

    Feat: 리프레시 토큰 유효성 검사

---
+++ src/product/auth/service/auth.createAccessToken.js
@@ -0,0 +1,21 @@
+import jwt from 'jsonwebtoken';
+import authHelp from './auth.help.service';
+import userHelp from '../../user/service/user.help.service';
+import ErrorMessage from '../../help/exception';
+
+const getRefreshTokenHash = async (token) => {
+  if (token === undefined) {
+    throw new ErrorMessage.UndefinedToken();
+  }
+  return jwt.verify(
+    token.split('Bearer ')[1],
+    process.env.REFRESH_JWT_KEY,
+    (err, payload) => {
+      if (!err) {
+        return payload.hash;
+      } else {
+        throw new ErrorMessage.ExpiredToken('유효하지 않은 토큰입니다.');
+      }
+    }
+  );
+};
