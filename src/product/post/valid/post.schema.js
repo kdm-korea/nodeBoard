bf26f82cf047707c152ee8d5310d74d893928a23
KDM_korea <kdm_korea@naver.com>
2020-06-09 17:37:14 +0900

    Feat:  유효성 검사 스키마 추가

--- src/product/post/valid/post.schema.js
+++ src/product/post/valid/post.schema.js
@@ -12,12 +12,18 @@ const validate = (validations) => {
   };
 };
 
-// const jwt = header('authorization')
-//   .not()
-//   .isEmpty()
-//   .withMessage('토큰이 없습니다.');
+const token = header('authorization')
+  .not()
+  .isEmpty()
+  .withMessage('토큰이 없습니다.');
 
-const id = param('id').isNumeric().withMessage('숫자로만 이루어져야 합니다.');
+const id = param('postId')
+  .isNumeric()
+  .withMessage('숫자로만 이루어져야 합니다.');
+
+const pageId = param('pageId')
+  .isNumeric()
+  .withMessage('숫자로만 이루어져 있어야 합니다.');
 
 const title = check('title')
   .not()
@@ -35,8 +41,9 @@ const password = check('password')
   .withMessage('비밀번호는 필수 입력사항입니다.');
 
 export default {
+  getPageSchema: validate([pageId]),
   getOneSchema: validate([id]),
-  updateSchema: validate([id, contents, title]),
-  postSchema: validate([title, contents]),
-  deleteSchema: validate([password, id]),
+  postSaveSchema: validate([token, title, contents]),
+  postPatchSchema: validate([token, id, contents, title]),
+  deleteSchema: validate([token, password, id]),
 };
