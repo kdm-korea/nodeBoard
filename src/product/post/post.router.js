a343fa06d509d8d48a9c5b1405d6f0563b8c2f4a
KDM_korea <kdm_korea@naver.com>
2020-06-09 17:35:48 +0900

    Fix: swagger doc 삭제

--- src/product/post/post.router.js
+++ src/product/post/post.router.js
@@ -1,50 +1,33 @@
 import express from 'express';
-import swaggerValidation from 'openapi-validator-middleware';
 import auth from '../../middleware/auth/jwt.auth';
 import valid from './valid/post.schema';
 import postController from './post.controller';
 
 const router = express.Router();
 
-swaggerValidation.init('src/product/post/valid/post.router.yaml');
+router.get('/post/page/:pageId', valid.getPageSchema, postController.paging);
 
-router.get('/post/page/:id', postController.paging);
-
-router.get(
-  '/post/:id',
-  // swaggerValidation.validate,
-  valid.getOneSchema,
-  postController.findOne
-);
+router.get('/post/:postId', valid.getOneSchema, postController.findOne);
 
 router.post(
   '/post',
-  // swaggerValidation.validate,
   auth.verification,
-  valid.postSchema,
+  valid.postSaveSchema,
   postController.createPost
 );
 
 router.patch(
-  '/post/:id',
+  '/post/:postId',
   auth.verification,
-  valid.updateSchema,
+  valid.postPatchSchema,
   postController.updatePost
 );
 
 router.delete(
-  '/post/:id',
+  '/post/:postId',
   auth.verification,
   valid.getOneSchema,
   postController.deletePost
 );
-
-router.use((err, req, res, next) => {
-  if (err instanceof swaggerValidation.InputValidationError) {
-    res.status(400).json(err.errors);
-  } else {
-    next();
-  }
-});
 
 module.exports = router;
