2e9d7f46c798822830388be8bdf23604c4c1ad61
KDM_korea <kdm_korea@naver.com>
2020-06-09 17:46:08 +0900

    BDD: Post create

---
+++ src/test/post/post.create.spec.js
@@ -0,0 +1,38 @@
+// import httpMocks from 'node-mocks-http';
+// import postRouter from '../../product/post/post.router';
+// import userController from '../../product/user/user.controller';
+
+// const signUpRequest = httpMocks.createRequest();
+// const singUpResponse = httpMocks.createResponse();
+
+// const signUpResult = false;
+// const signInResult = false;
+
+// const res = httpMocks.createResponse();
+
+// const req = httpMocks.createRequest({
+//   method: 'POST',
+//   baseUrl: process.env.HTTP_BASEURL,
+//   path: '/post',
+//   type: 'application/json',
+// });
+
+// req.body({
+//   title: '',
+//   contents: '',
+// });
+
+// // const
+
+// describe('글을 작성하여 업로드한다.', () => {
+//   before(() => {
+//     //TODO: 회원가입 => 로그인 => 토큰생성
+//   });
+
+//   it('정상적인 정보로 포스팅한 경우', () => {});
+
+//   it('토큰이 없는 상태에서 포스팅한 경우', () => {});
+
+//   it('정상적인 포스팅 객체를 전송하지 않는 경우', () => {});
+
+// });
