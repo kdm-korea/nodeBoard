4fc32d3c5503637cd277fd866ce015ae5e5348fe
KDM_korea <kdm_korea@naver.com>
2020-06-09 17:39:36 +0900

    Feat: 유효성 검사 스키마 추가, jwt를 통한 유저 정보 추가

--- src/product/user/user.router.js
+++ src/product/user/user.router.js
@@ -5,25 +5,53 @@ import userSchema from './valid/user.schema';
 
 const router = express.Router();
 
-router.post('/signin', userController.signIn);
+router.post('/signin', userSchema.signInSchema, userController.signIn);
 
 router.post('/signup', userSchema.signUpSchema, userController.signUp);
 
-router.post('/signout', jwt.verification, userController.signOut);
+router.post(
+  '/signout',
+  userSchema.signOutSchema,
+  jwt.verification,
+  userController.signOut
+);
 
-router.get('/user', jwt.verification, userController.userInfo);
+router.get(
+  '/user',
+  userSchema.userInfoSchema,
+  jwt.verification,
+  userController.userInfo
+);
 
-router.delete('/user', jwt.verification, userController.deleteUser);
+router.delete(
+  '/user',
+  userSchema.DeleteUserSchema,
+  jwt.verification,
+  userController.deleteUser
+);
 
-router.patch('/user', userController.modifyInfo);
+router.patch(
+  '/user',
+  userSchema.modifyInfoSchema,
+  jwt.verification,
+  userController.modifyInfo
+);
 
-// router.patch('/user/pw', userController.modifyPw);
+router.patch(
+  '/user/pw',
+  userSchema.modifyPwSchema,
+  jwt.verification,
+  userController.modifyPw
+);
 
-router.post('/user/pw', jwt.verification, userController.comparePassword);
+router.post(
+  '/user/pw',
+  userSchema.comparePassword,
+  jwt.verification,
+  userController.comparePassword
+);
 
-// 이메일로 인증번호 받기
-
-// 이메일 인증번호 확인
+// 이메일로 인증번호 받기, 이메일 인증번호 확인
 
 // 비밀번호 확인
 
