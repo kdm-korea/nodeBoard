318d57c18349ab391dcd160bd0a2b458d23289d8
KDM_korea <kdm_korea@naver.com>
2020-06-09 17:36:52 +0900

    Fix: Custom exception class로 변경

--- src/product/post/service/post.update.service.js
+++ src/product/post/service/post.update.service.js
@@ -1,11 +1,12 @@
 import db from '../../../config/mariadb.config';
 import postHelp from './post.help.service';
+import ErrorMessage from '../../help/exception';
 
 const execUpdatePost = async (postId, post, hash) => {
   const writer = (await postHelp.findOneById(postId)).User;
 
   if (writer.hash !== hash) {
-    throw new Error('다른 사용자입니다.');
+    throw new ErrorMessage.Forbioddan('다른 사용자입니다.');
   }
   return db.Board.update(
     { title: post.title, contents: post.contents },
