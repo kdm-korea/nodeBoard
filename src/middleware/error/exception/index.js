eec858f921657f7d2196c1f1e714c1297d8549ea
KDM_korea <kdm_korea@naver.com>
2020-06-09 17:26:54 +0900

    Fix: Error indexer path change

--- src/middleware/error/exception/index.js
+++
@@ -1,6 +0,0 @@
-import BaseError from './error.Base';
-import ConfictError from './error.Confict';
-import NotFoundUrl from './error.NotFoundUrl';
-import Unauthenticated from './error.Unauthenticated';
-
-export default { BaseError, ConfictError, NotFoundUrl, Unauthenticated };
---
+++ src/product/help/exception/index.js
@@ -0,0 +1,13 @@
+import ConfictError from './error.Confict';
+import NotFoundUrl from './error.NotFoundUrl';
+import Forbioddan from './error.Forbioddan';
+import ExpiredToken from './error.ExpiredToken';
+import UndefinedToken from './error.UndefinedToken';
+
+export default {
+  ConfictError,
+  NotFoundUrl,
+  Forbioddan,
+  ExpiredToken,
+  UndefinedToken,
+};
