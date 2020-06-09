524b07486caa9e27734bf9db038e2cb4f4a17d08
KDM_korea <kdm_korea@naver.com>
2020-06-09 17:46:14 +0900

    BDD: Post findOne

---
+++ src/test/post/post.findOne.spec.js
@@ -0,0 +1,24 @@
+// import httpMocks from 'node-mocks-http';
+// import postRouter from '../../product/post/post.router';
+// import should from 'should';
+// import { EventEmitter } from 'events';
+
+// const res = httpMocks.createResponse({
+//   eventEmitter: EventEmitter,
+// });
+
+// const req = httpMocks.createRequest({
+//   method: 'GET',
+//   baseUrl: 'http://localhost:8080',
+//   path: '/post/2',
+//   type: 'application/json',
+// });
+
+// describe('Get post find one', () => {
+//   it('Is Correct return Post', () => {
+//     postRouter(req, res);
+//     // console.log(res);
+//   });
+
+//   it('IS ');
+// });
