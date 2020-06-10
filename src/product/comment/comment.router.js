6fd4565b9975e94b6ffea0fbd883681f69669154
KDM_korea <kdm_korea@naver.com>
2020-06-10 20:29:29 +0900

    Feat: Commnet Rotuer add

---
+++ src/product/comment/comment.router.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import jwt from '../../middleware/auth/jwt.auth';
+import commentController from './comment.controller';
+
+const router = express.Router();
+
+//base router = post/:postId
+router.get('post/:postId/comment/page/:pageId');
+
+router.post(
+  '/post/:postId/comment',
+  jwt.verification,
+  commentController.createComment
+);
+
+router.patch('/comment/:commentId', jwt.verification);
+
+router.delete('/comment/:commentId', jwt.verification);
+
+module.exports = router;
