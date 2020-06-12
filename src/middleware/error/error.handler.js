187c91ab67d7c4a7aa31550ec0757b29da0eec84
KDM_korea <kdm_korea@naver.com>
2020-06-12 19:04:27 +0900

    Faet: Syntax Error 처리 추가

--- src/middleware/error/error.handler.js
+++ src/middleware/error/error.handler.js
@@ -6,6 +6,8 @@ const ErrorHandler = async (err, req, res, next) => {
   if (err instanceof BaseError) {
     console.log('Custom Error Exception');
     res.status(err.status).json(err);
+  } else if (err instanceof SyntaxError) {
+    res.status(400).json({ message: 'body syntex error' });
   } else {
     res.status(500).json({ errorMessage: 'Server Error' });
     console.log('UnException Error Handling');
