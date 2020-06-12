6857a50abb8f1b07e48952e5d92c600e1a5da4ab
KDM_korea <kdm_korea@naver.com>
2020-06-12 15:12:39 +0900

    Fix: 스키마 체이닝을 위한 함수를 help 폴더로 이동

--- src/product/post/valid/post.schema.js
+++ src/product/post/valid/post.schema.js
@@ -1,16 +1,5 @@
-import { check, validationResult, param, header } from 'express-validator';
-
-const validate = (validations) => {
-  return async (req, res, next) => {
-    await Promise.all(validations.map((validation) => validation.run(req)));
-
-    const errors = validationResult(req);
-    if (errors.isEmpty()) {
-      return next();
-    }
-    res.status(422).json({ errors: errors.array() });
-  };
-};
+import { check, param, header } from 'express-validator';
+import validate from '../../help/validChining';
 
 const token = header('authorization')
   .not()
