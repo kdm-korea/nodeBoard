bbdf96e822b31eb06132b4849ce247a8628df876
KDM_korea <kdm_korea@naver.com>
2020-06-12 15:11:09 +0900

    Fix: 인자가 하나라  Dto를 사용할 필요가 없음

--- src/product/comment/service/comment.delete.service.js
+++ src/product/comment/service/comment.delete.service.js
@@ -6,9 +6,9 @@ const deleteComment = async (commentId) => {
   return db.Comment.destroy({ where: { id: commentId } });
 };
 
-const execDeleteComment = async (hash, dto) => {
-  if (await commentHelp.compareCommentWriterHash(dto.commentId, hash)) {
-    return deleteComment(dto.commentId).then((count) => {
+const execDeleteComment = async (hash, commentId) => {
+  if (await commentHelp.compareCommentWriterHash(commentId, hash)) {
+    return deleteComment(commentId).then((count) => {
       if (count === 1) {
         return true;
       }
