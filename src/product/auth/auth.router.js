a518bf402a68a35655910c33d83d53eeb0d37a74
KDM_korea <kdm_korea@naver.com>
2020-06-12 16:11:42 +0900

    Feat: 토큰 인증 유효성 검사 추가

--- src/product/auth/auth.router.js
+++ src/product/auth/auth.router.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import authController from './auth.contorller';
+import authSchema from './valid/auth.schema';
 
 const router = express.Router();
 
-router.post('/auth', authController.createAccessToken);
+router.post(
+  '/auth',
+  authSchema.createAccessToken,
+  authController.createAccessToken
+);
 
 module.exports = router;
